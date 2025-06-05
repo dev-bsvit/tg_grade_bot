59429252f7498bb1e5e445375b45e641f188a17b
dev-bsvit <designbsvit@gmail.com>
2025-06-05 11:18:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -525,8 +525,6 @@ async def main():
 
     await application.run_polling()
 
-if __name__ == "__main__":
-    import nest_asyncio
+if __name__ == '__main__':
     import asyncio
-    nest_asyncio.apply()
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
