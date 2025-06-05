73005426366e7db6976d77f3514719516f9dda16
dev-bsvit <designbsvit@gmail.com>
2025-06-05 12:48:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -525,8 +525,9 @@ async def main():
     await start_health_server()
     await application.run_polling()
 
-if __name__ == '__main__':
-    import asyncio
+if __name__ == "__main__":
     import nest_asyncio
     nest_asyncio.apply()
-    asyncio.get_event_loop().run_until_complete(main())
+
+    import asyncio
+    asyncio.run(main())
